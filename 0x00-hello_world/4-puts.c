faa6d1972f0f2b843f2fdc5e728f43871dd0a99c
Tomisin510 <99489864+Tomisin510@users.noreply.github.com>
2022-03-10 15:33:27 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,17 +7,6 @@
 
 int main(void)
 {
-	/*var declaration*/
-	char varchar;
-	int varint;
-	long varlong;
-	long long varlongl;
-	float varfloat;
-	/*printing vars*/
-	printf("Size of a char: %d byte(s)\n", sizeof(varchar));
-	printf("Size of an int: %d byte(s)\n", sizeof(varint));
-	printf("Size of a long int: %d byte(s)\n", sizeof(varlong));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(varlongl));
-	printf("Size of a float: %d byte(s)\n", sizeof(varfloat));
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
