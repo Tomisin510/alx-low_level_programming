8db0a5f6e9b1eb448dda809ea71bca63541b3a9d
Tomisin510 <99489864+Tomisin510@users.noreply.github.com>
2022-03-10 23:01:23 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -7,6 +8,8 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	
 	return (1);
 }
