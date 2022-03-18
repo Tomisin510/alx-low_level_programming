172b4a2ea55553b9f0281d129c4a0ba19d90364a
Tomisin510 <99489864+Tomisin510@users.noreply.github.com>
2022-03-18 03:22:18 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,11 @@
 void print_line(int n)
 {
 	int i;
-	char k;
 
-	k = '_';
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (n > 0)
-			_putchar(k);
+		for (i = 0; i < n; i++)
+			_putchar('_');
 	}
 	_putchar('\n');
 }
